887f4483b39869d49abd702c0f8717eed67c4568
GGraziadei <48211703+GGraziadei@users.noreply.github.com>
2019-08-05 03:35:08 +0200

    Create rate.php

---
+++ rate.php
@@ -0,0 +1,20 @@
+<?php 
+
+add_filter('woocommerce_thankyou_order_received_text', 'woo_change_order_received_text', 10, 2 );
+function woo_change_order_received_text( $str, $order ) {
+    $new_str = $str . '<br>
+    Lascia una recensione sul servizio offerto.
+<div class="rating" data-rate-value=6></div>
+<script>
+$(".rating").rate();
+
+//or for example
+var options = {
+    max_value: 6,
+    step_size: 0.5,
+}
+$(".rating").rate(options);
+</script>
+    ';
+    return $new_str;
+}
